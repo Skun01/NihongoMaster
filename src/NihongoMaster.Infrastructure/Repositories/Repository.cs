47336dd708da139d21be64708c701842a3b3cf24
Skun01 <truongg9655@gmail.com>
2025-11-21 23:54:21 +0700

    implement base repository

---
+++ src/NihongoMaster.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using NihongoMaster.Application.Interfaces;
+using NihongoMaster.Infrastructure.Data;
+
+namespace NihongoMaster.Infrastructure.Repositories;
+
+public class Repository<T> : IRepository<T> where T : class
+{
+
+    protected readonly ApplicationContext _context;
+    internal DbSet<T> dbSet;
+    public Repository(ApplicationContext context)
+    {
+        _context = context;
+        this.dbSet = _context.Set<T>();
+    }
+
+    public async Task AddAsync(T entity) => await dbSet.AddAsync(entity);
+
+    public Task DeleteAsync(T entity)
+    {
+        dbSet.Remove(entity);
+        return Task.CompletedTask;
+    }
+
+    public async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate) => await dbSet.Where(predicate).ToListAsync();
+
+    public async Task<IEnumerable<T>> GetAllAsync() => await dbSet.ToListAsync();
+
+    public async Task<T?> GetByIdAsync(int id) => await dbSet.FindAsync(id);
+
+    public Task UpdateAsync(T entity)
+    {
+        dbSet.Update(entity);
+        return Task.CompletedTask;
+    }
+}
